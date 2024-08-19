8e12a67fd79ddca3633a916bbcedf0a695742a60
alex8396 <alex8396@naver.com>
2024-08-20 01:28:43 +0900

    완성!

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { footerText } from "@/constants";
+
+const Footer = () => {
+    return (
+        <footer id="footer" role="contentinfo">
+            <div className="footer__inner">
+                <h2 className="footer__text">
+                    <div>limjaechan</div>
+                    <div>@webs</div>
+                </h2>
+                <div className="footer__info">
+                    <div className="left">
+                        
+                    </div>
+                    <div className="right">
+                        <h3>social</h3>
+                        <ul>
+                            {footerText.map((footer, key) => (
+                                <li key={key}>
+                                    <a href={footer.link}>{footer.title}</a>
+                                    <em>{footer.desc}</em>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+                <div className="footer__right">
+                    &copy; 2024 limjaechan<br />
+                    이 사이트는 넥스트를 이용하여 제작하였습니다.
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
