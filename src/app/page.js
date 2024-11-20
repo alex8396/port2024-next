b3b5a302006c76bb14b0fe5f211009c0e138bdae
alex8396 <alex8396@naver.com>
2024-11-21 02:13:03 +0900

    dd

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Skip from "@/components/Skip";
 import Intro from "@/components/Intro";
 import Skill from "@/components/Skill";
 import Site from "@/components/Site";
-import Port from "@/components/Port";
+
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 import smooth from "@/utils/smooth";
@@ -26,7 +26,7 @@ export default function Home(){
                 <Intro />
                 <Skill />
                 <Site />
-                <Port />
+                
                 <Contact />
             </main>
             <Footer />
